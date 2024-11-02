af634524c57d4a194cbc31c65659eb6d66755c31
Khyati <ksisodia.2201@gmail.com>
2024-11-03 02:34:01 +0530

    Deploy documentation to GitHub Pages

---
+++ source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'IES'
+copyright = '2024, Khyati'
+author = 'Khyati'
+release = '2024'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'English'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+
+
+html_static_path = ['_static']
+html_logo = "_static/ies_logo.jpeg"
+
+html_css_files = ['custom.css']
